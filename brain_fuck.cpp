8dfd9dcd39e71dc05fba8d28763f0da429ccf3b4
lylanfree <xdds_li@163.com>
2020-12-04 21:12:11 +0800

    add error process

--- brain_fuck.cpp
+++ brain_fuck.cpp
@@ -5,40 +5,55 @@
 #include <unordered_map>
 
 const int MAX_NUM = 30000;
+const int INVALID_INDEX = -2;
 
 std::unordered_map<int, int> brack_map;
 std::array<char, MAX_NUM> init() { return std::array<char, MAX_NUM>{{0}}; }
 
-int find_next(std::string &inputs, int index) {
+int find_match(std::string &inputs, int index, char c) {
   auto search = brack_map.find(index);
-  if (search != brack_map.end()) return search->second;
+  if (search != brack_map.end())
+    return search->second;
+  if (c == ']') {
+    std::cerr << "Input Error, there is no match with char \'" << c
+              << "\' in position:" << index + 1 << std::endl;
+    return INVALID_INDEX;
+  }
   int begin = index, num = 1;
   while (++index < inputs.length()) {
     if (inputs[index] == '[') {
       num++;
     } else if (inputs[index] == ']') {
       num--;
-      if (num == 0) {
-        brack_map.insert({begin, index - 1});
-        brack_map.insert({index, begin - 1});
-        return index - 1;
-      }
+    }
+    if (num == 0) {
+      brack_map.insert({begin, index - 1});
+      brack_map.insert({index, begin - 1});
+      return index - 1;
+    } else if (num < 0) {
+      break;
     }
   }
-  throw "Inputs is invalid\n";
+  std::cerr << "Input Error, there is no match with char \'" << c
+            << "\' in position:" << index + 1 << std::endl;
+  return INVALID_INDEX;
 }
 
 int main(int argc, char *argv[]) {
   std::array<char, MAX_NUM> nums = init();
-  std::string inputs;
+  std::string inputs, line;
   int index = 0;
   std::ifstream file("inputs.txt");
   if (file.is_open()) {
-    std::getline(file, inputs);
+    while (std::getline(file, line)) {
+      if (!line.empty()) {
+        inputs += line;
+      }
+    }
   }
   for (int i = 0; i < inputs.length(); i++) {
-    char num = inputs[i];
-    switch (num) {
+    char c = inputs[i];
+    switch (c) {
       case '+':
         nums[index]++;
         break;
@@ -49,28 +64,40 @@ int main(int argc, char *argv[]) {
         if (index < MAX_NUM - 1) {
           index++;
         } else {
-          goto Exception;
+          std::cerr
+              << "Compute error, beyond the upper bound of array in position:"
+              << i + 1 << std::endl;
+          return -2;
         }
         break;
       case '<':
         if (index > 0) {
           index--;
         } else {
-          goto Exception;
+          std::cerr
+              << "Compute error, below the lower bound of array in position:"
+              << i + 1 << std::endl;
+          return -2;
         }
         break;
       case '.':
         std::cout << nums[index];
         break;
       case '[':
-        find_next(inputs, i);
+        find_match(inputs, i, c);
         if (nums[index] == 0) {
-          i = find_next(inputs, i);
+          i = find_match(inputs, i, c);
+          if (i == INVALID_INDEX) {
+            return -1;
+          }
         }
         break;
       case ']':
         if (nums[index] != 0) {
-          i = find_next(inputs, i);
+          i = find_match(inputs, i, c);
+          if (i == INVALID_INDEX) {
+            return -1;
+          }
         }
         break;
       case ',':
@@ -81,6 +108,4 @@ int main(int argc, char *argv[]) {
     }
   }
   return 0;
-Exception:
-  throw "Inputs is invalid\n";
 }
